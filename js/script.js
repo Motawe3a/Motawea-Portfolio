74d584608a541e5122b64373cecb4a9d4f6303cd
aabdelmohsen.c <aabdelmohsen.c@tahakom.com>
2026-09-12 16:53:33 +0300

    add missed projects, fix responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector(".nav");
 const navList = nav.querySelectorAll("li");
+const aside = document.querySelector(".aside");
+const navToggler = document.querySelector(".nav-toggler");
 
 for (let i = 0; i < navList.length; i++) {
     const a = navList[i].querySelector("a")
@@ -7,6 +9,11 @@ for (let i = 0; i < navList.length; i++) {
         for (let j = 0; j < navList.length; j++) {
             navList[j].querySelector('a').classList.remove("active");
         }
-        this.classList.add("active")
+        a.classList.add("active")
+        aside.classList.remove("open");
     });
 }
+
+navToggler.addEventListener("click", () => {
+    aside.classList.toggle("open");
+});
